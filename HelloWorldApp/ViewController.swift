2715624e3db83f18fc878fd7e8efbaee658ad854
Dennis Nesanoff <nndennisnn@gmail.com>
2019-09-30 23:51:06 +0300

    Initial Commit

---
+++ HelloWorldApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWorldApp
+//
+//  Created by Dennis Nesanoff on 30/09/2019.
+//  Copyright © 2019 Dennis Nesanoff. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
