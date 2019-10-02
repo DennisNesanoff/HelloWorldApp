6ade14671929d1d35dc872c300a0edb9fdae24ac
Dennis Nesanoff <nndennisnn@gmail.com>
2019-10-02 07:33:42 +0300

    final version

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var text: UILabel!
+    @IBOutlet var button: UIButton!
+    
+    // свойства
+    // private
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        text.isHidden = true
+        button.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func showAndHigthText() {
+        text.isHidden = !text.isHidden
+        if text.isHidden {
+            button.setTitle("Show text", for: .normal)
+        } else {
+            button.setTitle("Hight text", for: .normal)
+        }
+        
+        
+    }
+    // metods
+    // private func
 }
 
